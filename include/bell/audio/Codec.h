6a7f4d368286149144312ee1ea14a1e62760d076
Filip <f.grzywok@gmail.com>
2024-12-11 13:47:21 +0100

    feat: Document missing functions in the codec abstraction

--- include/bell/audio/Codec.h
+++ include/bell/audio/Codec.h
@@ -48,9 +48,28 @@ class Codec {
    */
   bell::audio::Format getAudioFormat() const { return this->audioFormat; }
 
+  /**
+   * @brief Encodes the input PCM data, returning the encoded data
+   * 
+   * @param pcmInput Pointer to the input PCM data, in the format specified by the audioFormat
+   * @param inputLength Length of the input PCM data
+   * @param outputLength Pointer to size_t to store the length of the encoded data
+   * @param result Result code of the operation, indicating success, error, or need for more data
+   * @return uint8_t* Pointer to the encoded data, or nullptr if an error occurred
+   */
   virtual uint8_t* encode(const uint8_t* pcmInput, size_t inputLength,
                           size_t& outputLength, ResultCode& result) = 0;
-  virtual uint8_t* decode(const uint8_t* pcmInput, size_t inputLength,
+
+  /**
+   * @brief Decodes the input encoded data, returning the decoded PCM data
+   * 
+   * @param encodedInput Pointer to the input encoded data
+   * @param inputLength Length of the input encoded data
+   * @param outputLength Pointer to size_t to store the length of the decoded PCM data
+   * @param result Result code of the operation, indicating success, error, or need for more data
+   * @return uint8_t* Pointer to the decoded PCM data, or nullptr if an error occurred
+   */
+  virtual uint8_t* decode(const uint8_t* encodedInput, size_t inputLength,
                           size_t& outputLength, ResultCode& result) = 0;
 
  protected:
