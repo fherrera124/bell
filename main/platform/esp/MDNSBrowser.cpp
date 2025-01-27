71a21913e2b11bce636b0f605df375e2edce5fc3
Filip Grzywok <f.grzywok@gmail.com>
2025-01-27 17:39:42 +0100

    feat: begin implementing mdnsbrowser for espressif

--- main/platform/esp/MDNSBrowser.cpp
+++ main/platform/esp/MDNSBrowser.cpp
@@ -1,17 +1,140 @@
 #include "bell/mdns/Browser.h"
 
 // Standar includes
-#include <algorithm>
-#include <iostream>
-#include <regex>
+#include <array>
 #include <stdexcept>
 #include <string>
 
+// Library includes
+#include "esp_netif_ip_addr.h"
+#include "lwip/ip_addr.h"
+#include "mdns.h"
+
+// Bell includes
+#include "bell/Logger.h"
+
 using namespace bell;
+
+class implMDNSBrowser : public mdns::Browser {
+ public:
+  implMDNSBrowser(std::string regType, std::string regDomain,
+                  int interfaceIndex, DiscoveryEventCallback onEvent,
+                  bool autoResolveService = true, bool resolveIpv6 = true)
+      : regType(std::move(regType)),
+        regDomain(std::move(regDomain)),
+        onEvent(std::move(onEvent)) {
+    // Extract the protocol from the regType
+    serviceType = regType.substr(0, regType.find_first_of('.'));
+    proto = regType.substr(regType.find_first_of('.') + 1);
+  }
+
+  // Delete copy constructor and copy assignment operator
+  implMDNSBrowser(const implMDNSBrowser&) = delete;
+  implMDNSBrowser& operator=(const implMDNSBrowser&) = delete;
+
+  ~implMDNSBrowser() override { stopDiscovery(); }
+
+ private:
+  const char* LOG_TAG = "EspressifMDNSBrowser";
+
+  std::string regType;
+  std::string regDomain;
+  std::string proto;
+  std::string serviceType;
+  DiscoveryEventCallback onEvent;
+
+  const int maxResults = 32;
+
+  std::vector<DiscoveredRecord> discoveredRecords;
+
+  std::vector<DiscoveredRecord> parseResults(mdns_result_t* results) {
+    mdns_result_t* r = results;
+    mdns_ip_addr_t* a = nullptr;
+    std::vector<DiscoveredRecord> records;
+
+    while (r) {
+      DiscoveredRecord record;
+
+      record.interfaceIndex = r->esp_netif
+
+      if (r->instance_name) {
+        record.name = r->instance_name;
+        record.serviceResolved = true;
+      }
+
+      if (r->hostname) {
+        record.hostname = r->hostname;
+        record.port = r->port;
+        record.addressResolved = true;
+      }
+
+
+
+      a = r->addr;
+      while (a) {
+        if (a->addr.type == IPADDR_TYPE_V4) {
+          std::array<char, IP4ADDR_STRLEN_MAX> strCharData{};
+          esp_ip4addr_ntoa(&a->addr.u_addr.ip4, strCharData.data(),
+                           IP4ADDR_STRLEN_MAX);
+          std::string ipStr(strCharData.data());
+          record.addresses.push_back(net::IpAddress::fromString(ipStr));
+        } else if (a->addr.type == IPADDR_TYPE_V6) {
+          // TODO: Implement IPv6 support
+          //   std::array<char, IP6ADDR_STRLEN_MAX> strCharData{};
+          //   esp_ip6addr(&a->addr.u_addr.ip6, strCharData.data(),
+          //                    IP6ADDR_STRLEN_MAX);
+          //   std::string ipStr(strCharData.data());
+          //   record.addresses.push_back(net::IpAddress::fromString(ipStr));
+          // }
+        }
+        a = a->next;
+      }
+
+      // copy txt records int std::unordered_map
+      for (int x = 0; x < r->txt_count; x++) {
+        record.txtRecords.insert(
+            {std::string(r->txt[x].key),
+             std::string(&r->txt[x].value[0],
+                         &r->txt[x].value[r->txt_value_len[x]])});
+      }
+
+      records.push_back(record);
+      r = r->next;
+    }
+
+    publishDiscovered();
+  }
+
+  void processEvents(int timeoutMs) override {
+    // Start Avahi thread poll
+    mdns_result_t* results = nullptr;
+    esp_err_t err = mdns_query_ptr(serviceType.c_str(), proto.c_str(),
+                                   timeoutMs, maxResults, &results);
+    if (err) {
+      throw std::runtime_error("Could not query mdns services");
+    }
+
+    processResults(results);
+
+    mdns_query_results_free(results);
+  }
+
+  void stopDiscovery() override {}
+
+  void resolveService(const DiscoveredRecord& /*service*/) override {
+    // There is no need to manually resolve the service with espressif mdns, it is done automatically
+  }
+
+  void resolveAddress(const DiscoveredRecord& /*service*/) override {
+    // There is no need to manually resolve the address with espressif mdns, it is done automatically
+  }
+};
 
 std::unique_ptr<mdns::Browser> mdns::Browser::startDiscovery(
     const std::string& regType, const std::string& regDomain,
     int interfaceIndex, const DiscoveryEventCallback& onEvent,
     bool autoResolveService, bool /*autoResolveAddresses*/, bool resolveIpv6) {
-  return {};
+  return std::make_unique<implMDNSBrowser>(regType, regDomain, interfaceIndex,
+                                           onEvent, autoResolveService,
+                                           resolveIpv6);
 }
--- main/platform/esp/MDNSService.cpp
+++ main/platform/esp/MDNSService.cpp
@@ -1,23 +1,63 @@
 #include "bell/mdns/Service.h"
 
+// System includes
 #include <cassert>
-#include <functional>
+#include <string>
+#include <utility>
 #include <vector>
 
 // Own includes
-#include "bell/Logger.h"
-#include "bell/utils/Semaphore.h"
+#include "mdns.h"
 
 using namespace bell;
 
+// Espressif implementation of mdns::Service
+class implMDNSService : public mdns::Service {
+ public:
+  implMDNSService(std::string type, std::string proto,
+                  std::vector<mdns_txt_item_t> txtItems)
+      : type(std::move(type)),
+        proto(std::move(proto)),
+        txtItems(std::move(txtItems)) {}
+  ~implMDNSService() override { unregisterService(); }
+
+  // Delete copy constructor and copy assignment operator
+  implMDNSService(const implMDNSService&) = delete;
+  implMDNSService& operator=(const implMDNSService&) = delete;
+
+  void unregisterService() override {
+    mdns_service_remove(type.c_str(), proto.c_str());
+  }
+
+ private:
+  std::string type;
+  std::string proto;
+  std::vector<mdns_txt_item_t> txtItems;
+};
+
 /**
- * Avahi implementation of mdns::Service.
- * @see https://avahi.org/doxygen/html/
- * @remark This implementation does not handle collisions between services.
+ * ESP32 implementation of MDNSService
+ * @see https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/protocols/mdns.html
  **/
 std::unique_ptr<mdns::Service> mdns::Service::registerService(
     const std::string& serviceName, const std::string& serviceType,
     const std::string& serviceProto, const std::string& serviceHost,
     int servicePort, const std::map<std::string, std::string>& txtData) {
-  return {};
+  std::vector<mdns_txt_item_t> txtItems;
+  txtItems.reserve(txtData.size());
+  for (const auto& data : txtData) {
+    mdns_txt_item_t item;
+    item.key = data.first.c_str();
+    item.value = data.second.c_str();
+    txtItems.push_back(item);
+  }
+
+  mdns_service_add(serviceName.c_str(),  /* instance_name */
+                   serviceType.c_str(),  /* service_type */
+                   serviceProto.c_str(), /* proto */
+                   servicePort,          /* port */
+                   txtItems.data(),      /* txt */
+                   txtItems.size()       /* num_items */
+  );
+  return std::make_unique<implMDNSService>(serviceType, serviceProto, txtItems);
 }
