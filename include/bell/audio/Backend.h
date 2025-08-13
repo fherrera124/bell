14f3ea1725285a3d2cca85e76ad71f046dbdde8d
Filip Grzywok <f.grzywok@gmail.com>
2025-08-13 16:28:23 +0200

    feat: start preparing the new portaudio backend

--- include/bell/audio/Backend.h
+++ include/bell/audio/Backend.h
@@ -2,6 +2,7 @@
 
 // Standard includes
 #include <any>
+#include <memory>
 
 // Own includes
 #include "bell/Result.h"
@@ -19,16 +20,67 @@ class Backend {
 
   // Represents a single audio backend device
   struct Device {
-    uint8_t id;
+    uint32_t id;
     std::string name;
-    bool isDefault;
+
+    bool isDefaultInput{};
+    bool isDefaultOutput{};
+    int maxInputChannels{};
+    int maxOutputChannels{};
+    std::any backendSpecificContext;
   };
 
   // Represents the direction of the audio stream.
-  enum class StreamType {
+  enum class StreamType : std::uint8_t {
     Output,
     Input,
     Duplex  // Both input and output
+  };
+
+  class Stream {
+   public:
+    virtual ~Stream() = default;
+    Stream() = default;
+
+    Stream(const Stream&) = delete;
+    Stream& operator=(const Stream&) = delete;
+
+    /**
+     * @brief Starts the audio stream, which begins invoking the callback.
+     * @return A std::error_code if starting the stream fails.
+     */
+    virtual bell::Result<> start() = 0;
+
+    /**
+     * @brief Stops the audio stream, pausing the callback.
+     * @return A std::error_code if stopping the stream fails.
+     */
+    virtual bell::Result<> stop() = 0;
+
+    /**
+     * @brief Checks if a stream is currently open.
+     */
+    virtual bool isOpen() const = 0;
+
+    /**
+     * @brief Checks if the stream is currently running (i.e., started and not stopped).
+     */
+    virtual bool isStarted() const = 0;
+
+    /**
+     * @brief Gets the format of the audio stream.
+     */
+    virtual AudioFormat getFormat() const = 0;
+
+    /**
+     * @brief Gets the actual latency of the stream in frames.
+     */
+    virtual float getLatency() const = 0;
+
+    /**
+     * @brief Gets the type of the audio stream.
+     */
+    virtual StreamType getType() const = 0;
   };
 
   using AudioCallback = std::function<void(
@@ -42,81 +94,41 @@ class Backend {
    * @brief Gets a list of all available audio output devices.
    * @return A vector of AudioDevice objects.
    */
-  virtual std::vector<Device> getOutputDevices() const = 0;
+  virtual std::vector<Device> getOutputDevices() = 0;
 
   /**
    * @brief Gets a list of all available audio input devices.
    * @return A vector of AudioDevice objects.
    */
-  virtual std::vector<Device> getInputDevices() const = 0;
+  virtual std::vector<Device> getInputDevices() = 0;
 
   /**
    * @brief Gets the default output device as determined by the system.
    * @return The default AudioDevice.
    */
-  virtual bell::Result<Device> getDefaultOutputDevice() const = 0;
+  virtual bell::Result<Device> getDefaultOutputDevice() = 0;
 
   /**
    * @brief Gets the default output device as determined by the system.
    * @return The default AudioDevice.
    */
-  virtual bell::Result<Device> getDefaultInputDevice() const = 0;
+  virtual bell::Result<Device> getDefaultInputDevice() = 0;
 
   /**
    * @brief Opens an audio stream with the specified parameters.
    *
-   * @param outputParams Parameters for the output stream.
-   * @param inputParams Parameters for the input stream.
-   * @param sampleRate The desired sample rate (e.g., 44100, 48000).
+   * @param streamType Type of audio stream (input / output)
+   * @param format desired audio format of the stream
    * @param bufferFrames The desired number of frames for the internal buffer. Affects latency.
    * @param callback The user-defined function to be called for audio processing.
-   * @param userData A pointer to user data that will be passed to the callback.
    * @param backendSpecificOptions A type-erased object containing options for a specific backend.
    * The caller is responsible for passing a struct that the target backend understands.
    * @return A std::error_code if opening the stream fails.
    */
-  virtual bell::Result<> openStream(
-      StreamType streamType, const AudioFormat& format, unsigned int sampleRate,
-      uint32_t bufferFrames, AudioCallback callback,
-      const std::any& backendSpecificOptions = {}) = 0;
-
-  /**
-   * @brief Closes the currently open audio stream.
-   */
-  virtual void closeStream() = 0;
-
-  /**
-   * @brief Starts the audio stream, which begins invoking the callback.
-   * @return A std::error_code if starting the stream fails.
-   */
-  virtual bell::Result<> startStream() = 0;
-
-  /**
-   * @brief Stops the audio stream, pausing the callback.
-   * @return A std::error_code if stopping the stream fails.
-   */
-  virtual bell::Result<> stopStream() = 0;
-
-  /**
-   * @brief Checks if a stream is currently open.
-   */
-  virtual bool isStreamOpen() const = 0;
-
-  /**
-   * @brief Checks if the stream is currently running (i.e., started and not stopped).
-   */
-  virtual bool isStreamRunning() const = 0;
-
-  /**
-   * @brief Gets the actual sample rate of the open stream.
-   * This may differ from the requested rate if the hardware doesn't support it.
-   */
-  virtual SampleRate getStreamSampleRate() const = 0;
-
-  /**
-   * @brief Gets the actual latency of the stream in frames.
-   */
-  virtual double getStreamLatency() const = 0;
+  virtual bell::Result<std::unique_ptr<Stream>> openStream(
+      const Device& device,
+      StreamType streamType, const AudioFormat& format, uint32_t bufferFrames,
+      AudioCallback callback, const std::any& backendSpecificOptions = {}) = 0;
 };
 
 Backend* getDefaultAudioBackend();
@@ -129,5 +141,6 @@ namespace bell {
 using AudioBackend = audio::Backend;
 using AudioBackendDevice = audio::Backend::Device;
 using AudioBackendStreamType = audio::Backend::StreamType;
+using AudioBackendStream = audio::Backend::Stream;
 using AudioBackendCallback = audio::Backend::AudioCallback;
 }  // namespace bell
